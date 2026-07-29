fc0d7beaa519d887cdfe2eae25c1414bd1ea9f51
klawyis <Yenipes078@gmail.com>
2026-07-29 03:11:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,86 +5,70 @@ const gift = document.getElementById("gift");
 const giftScreen = document.getElementById("giftScreen");
 
 
-
 const messages = [
-
-"Twin...",
-
-"Uzakta olduğum için elimden anca bu geldi.",
-
-"2 yıldır hayatımı güzelleştiren yegane insansın.",
-
-"Ve nice mutlu senelere..",
-
-"Yada daha iyisi...",
-
-"Nice bizli senelere diyorum.",
-
-"İyi ki doğdun Twin 💚"
-
+    "Twin...",
+    "Uzakta olduğum için elimden anca bu geldi.",
+    "2 yıldır hayatımı güzelleştiren yegane insansın.",
+    "Ve nice mutlu senelere..",
+    "Yada daha iyisi...",
+    "Nice bizli senelere diyorum.",
+    "İyi ki doğdun twin 💚"
 ];
 
 
 
-function wait(ms){
+function wait(time){
 
-return new Promise(resolve=>setTimeout(resolve,ms));
+    return new Promise(function(resolve){
 
-}
+        setTimeout(resolve,time);
 
-
-
-
-async function write(message){
-
-
-text.innerHTML="";
-
-
-text.classList.remove("fade");
-
-
-void text.offsetWidth;
-
-
-text.classList.add("fade");
-
-
-
-for(let char of message){
-
-
-text.innerHTML += char;
-
-
-await wait(60);
-
-
-}
-
-
-await wait(2500);
-
+    });
 
 }
 
 
 
 
-async function start(){
+async function writeText(message){
+
+    text.innerHTML = "";
+
+    text.classList.remove("fade");
+
+    void text.offsetWidth;
+
+    text.classList.add("fade");
 
 
-for(let message of messages){
+    for(let i = 0; i < message.length; i++){
+
+        text.innerHTML += message[i];
+
+        await wait(70);
+
+    }
 
 
-await write(message);
-
+    await wait(2000);
 
 }
 
 
 
-loveQuestion();
+
+
+async function startStory(){
+
+
+    for(let i = 0; i < messages.length; i++){
+
+        await writeText(messages[i]);
+
+    }
+
+
+    askLove();
 
 
 }
@@ -94,197 +78,208 @@ loveQuestion();
 
 
 
-function loveQuestion(){
+function askLove(){
+
+
+    text.innerHTML = "Beni seviyor musun Twin? 💚";
+
+
+    buttons.innerHTML = 
+    `
+    <button id="yes">
+    EVET 💚
+    </button>
+
+    <button id="no">
+    HAYIR 😈
+    </button>
+    `;
 
 
 
-text.innerHTML="Beni seviyor musun Twin? 💚";
+    const yesButton = document.getElementById("yes");
+    const noButton = document.getElementById("no");
 
 
 
-buttons.innerHTML=`
-
-<button id="yes">
-EVET 💚
-</button>
+    function moveNoButton(){
 
 
-<button id="no">
-HAYIR 😈
-</button>
-
-`;
+        noButton.style.position = "fixed";
 
 
+        noButton.style.left =
+        Math.random() * 80 + "%";
 
-const yes=document.getElementById("yes");
 
-const no=document.getElementById("no");
+        noButton.style.top =
+        Math.random() * 80 + "%";
+
+
+    }
 
 
 
+    noButton.addEventListener(
+        "mouseenter",
+        moveNoButton
+    );
 
 
-function moveNo(){
+    noButton.addEventListener(
+        "click",
+        moveNoButton
+    );
 
 
-no.style.position="fixed";
 
 
-no.style.left=
-Math.random()*80+"%";
+
+    yesButton.addEventListener(
+        "click",
+        function(){
 
 
-no.style.top=
-Math.random()*80+"%";
+            buttons.innerHTML = "";
+
+
+            text.innerHTML =
+            "BİLİYORDUM TWIN 😎💚";
+
+
+            createHearts();
+
+
+
+            setTimeout(function(){
+
+
+                text.innerHTML =
+                "I LOVE YOU TWIN 💚";
+
+
+            },2500);
+
+
+
+            setTimeout(function(){
+
+
+                document.body.style.transition =
+                "3s";
+
+
+                document.body.style.opacity =
+                "0";
+
+
+            },6000);
+
+
+
+        }
+    );
+
 
 
 }
 
 
 
-no.addEventListener(
-"mouseenter",
-moveNo
+
+
+
+
+
+function createHearts(){
+
+
+    for(let i = 0; i < 60; i++){
+
+
+        let heart =
+        document.createElement("div");
+
+
+        heart.innerHTML = "💚";
+
+
+        heart.style.position = "fixed";
+
+
+        heart.style.left =
+        Math.random()*100 + "%";
+
+
+        heart.style.top =
+        Math.random()*100 + "%";
+
+
+        heart.style.fontSize =
+        "30px";
+
+
+        heart.style.animation =
+        "fall 3s linear";
+
+
+        document.body.appendChild(heart);
+
+
+
+        setTimeout(function(){
+
+            heart.remove();
+
+        },3000);
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+// HEDİYE KUTUSU AÇILIŞI
+
+
+gift.addEventListener(
+    "click",
+    function(){
+
+
+        gift.classList.add("open");
+
+
+
+        setTimeout(function(){
+
+
+            giftScreen.classList.add("hide");
+
+
+
+            setTimeout(function(){
+
+
+                startStory();
+
+
+            },1500);
+
+
+
+        },1000);
+
+
+
+    }
 );
-
-
-
-no.addEventListener(
-"click",
-moveNo
-);
-
-
-
-
-
-yes.onclick=function(){
-
-
-buttons.innerHTML="";
-
-
-text.innerHTML="BİLİYORDUM TWIN 😎💚";
-
-
-heartExplosion();
-
-
-
-setTimeout(()=>{
-
-
-text.innerHTML="I LOVE YOU TWIN 💚";
-
-
-},2500);
-
-
-
-setTimeout(()=>{
-
-
-document.body.style.transition="3s";
-
-
-document.body.style.opacity="0";
-
-
-},6000);
-
-
-
-};
-
-
-}
-
-
-
-
-
-
-
-function heartExplosion(){
-
-
-for(let i=0;i<50;i++){
-
-
-let h=document.createElement("div");
-
-
-h.innerHTML="💚";
-
-
-h.style.position="fixed";
-
-
-h.style.left=Math.random()*100+"%";
-
-
-h.style.top=Math.random()*100+"%";
-
-
-h.style.fontSize="30px";
-
-
-h.style.animation="fall 3s linear";
-
-
-document.body.appendChild(h);
-
-
-
-setTimeout(()=>{
-
-h.remove();
-
-},3000);
-
-
-}
-
-
-}
-
-
-
-
-
-
-
-
-// HEDİYE KUTUSU
-
-gift.onclick=function(){
-
-
-
-gift.classList.add("open");
-
-
-
-setTimeout(()=>{
-
-
-giftScreen.classList.add("hide");
-
-
-
-setTimeout(()=>{
-
-
-start();
-
-
-},1500);
-
-
-
-},1000);
-
-
-
-};
